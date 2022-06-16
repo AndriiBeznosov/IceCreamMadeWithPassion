eb1375b86ac4120d87008364b1b2de6606d2e5ce
AndriiBeznosov <99024313+AndriiBeznosov@users.noreply.github.com>
2022-06-16 15:06:53 +0000

    Deploying to gh-pages from @ AndriiBeznosov/IceCreamMadeWithPassion@e8ce2dd47e4b216720095b8e9ee46c12951b6768 🚀

--- index.a102eca6.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0}});
-//# sourceMappingURL=index.a102eca6.js.map
